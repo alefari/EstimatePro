b1c1e7f7d97e6941c7f7eba34e54438dfb9a4acc
alefari <ale.jf09@gmail.com>
2021-06-29 21:39:02 -0400

    Creada estructura componente presupuesto

--- src/app/components/presupuesto/presupuesto.component.ts
+++ src/app/components/presupuesto/presupuesto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item.models';
+import { CategoriasService } from 'src/app/services/categorias.service';
+import { ItemsService } from 'src/app/services/items.service';
+import { SubcategoriasService } from 'src/app/services/subcategorias.service';
 
 @Component({
   selector: 'app-presupuesto',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresupuestoComponent implements OnInit {
 
-  constructor() { }
+  listaItems: Item[];
+  listaCategorias: any[];
+  listaSubcategorias: any[];
+
+  itemsPresupuesto: Item[] = [];
+
+  constructor(private servicioCategorias: CategoriasService,
+              private servicioItems: ItemsService,
+              private servicioSubcategorias: SubcategoriasService) { }
 
   ngOnInit(): void {
+    this.servicioItems.obtenerItems().subscribe(items => {
+      this.listaItems = items.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
+    })
+    this.servicioCategorias.obtenerCategorias().subscribe(categorias => {
+      this.listaCategorias = categorias.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
+    })
+    this.servicioSubcategorias.obtenerSubcategorias().subscribe(subcategorias => {
+      this.listaSubcategorias = subcategorias.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1);
+    })
+  }
+
+  onSubmit() {
+
+  }
+
+  onAddItem(item: Item) {
+    this.itemsPresupuesto.push(item);
   }
 
 }
