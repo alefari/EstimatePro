19e8e00bf249bf893fd41c3a5d9fc0854ebb8732
alefari <ale.jf09@gmail.com>
2021-07-15 21:12:41 -0400

    visibilidad navbar y cambio a behaviorSubject

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(private servicioAuth: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.servicioAuth.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      // this.isAuthenticated = !user ? false : true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { environment } from 'src/environments/environment';
 import { catchError, tap } from 'rxjs/operators';
-import { Subject, throwError } from 'rxjs';
+import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from '../models/user.model';
 
 interface AuthResponseData {
@@ -19,7 +19,7 @@ interface AuthResponseData {
 })
 export class AuthService {
 
-  user = new Subject<User>();
+  user = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) { }
 
