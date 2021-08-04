986085e10ecfe331b4e5f9f1de6105f0fcfb582d
alefari <ale.jf09@gmail.com>
2021-08-03 21:39:53 -0400

    añade categorias a excel y fotmato a la tabla

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -20,8 +20,11 @@ export class ExcelService {
   testExcel(items: ItemPresupuesto[], presupuesto: Presupuesto, categoriasPresupuesto: any[], subcategoriasPresupuesto: any[]) {
     let itemsArray = [];
     let currentRow = 7;
+    let categoriasRows = [];
+    let subcategoriasRows = [];
+    console.log(items)
     items.forEach((objetoItem) => {
-      itemsArray.push([
+      let arrayFormulas = [
         objetoItem.L,
         objetoItem.M,
         objetoItem.nombre,
@@ -41,11 +44,112 @@ export class ExcelService {
         objetoItem.laborRateBase ? objetoItem.laborRateBase : 0,
         objetoItem.materialRateBase ? objetoItem.materialRateBase : 0,
         objetoItem.equipmentRateBase ? objetoItem.equipmentRateBase : 0,
-      ])
-      currentRow++;
+      ]
+      if(objetoItem.categoria == "" && objetoItem.subcategoria == "") {
+        itemsArray.push(arrayFormulas)
+        currentRow++;
+      }
     });
-
-    currentRow++;
+    subcategoriasPresupuesto.forEach((subcategoria) => {
+      if(items.some(item => item.categoria == "" && item.subcategoria == subcategoria)) {
+        itemsArray.push(["","",subcategoria,"","","","","","","","","","","","","","","",""]);
+        subcategoriasRows.push(currentRow);
+        currentRow++;
+        items.forEach((objetoItem) => {
+          let arrayFormulas = [
+            objetoItem.L,
+            objetoItem.M,
+            objetoItem.nombre,
+            objetoItem.qty,
+            objetoItem.unidad,
+            { formula: `Q${currentRow}*A${currentRow}`, result: objetoItem.productionRate },
+            { formula: `D${currentRow}*F${currentRow}`, result: objetoItem.laborHours },
+            { formula: `IF(Y4="Government",Z4,IF(T4=0,Q${currentRow}*A${currentRow},Q${currentRow}+Q${currentRow}*T4*0.01*A${currentRow}))`},
+            { formula: `IF(U4=0,R${currentRow}*B${currentRow},R${currentRow}+R${currentRow}*U4*0.01*B${currentRow})`},
+            { formula: `IF(V4=0,S${currentRow},S${currentRow}+S${currentRow}*V4*0.01)`},
+            { formula: `IF(Y4="Private",D${currentRow}*H${currentRow},G${currentRow}*Z4)`},
+            { formula: `D${currentRow}*I${currentRow}` },
+            { formula: `D${currentRow}*J${currentRow}+(D${currentRow}*J${currentRow}*X4)`},
+            { formula: `L${currentRow}+L${currentRow}*X4`},
+            objetoItem.estSubMarkup,
+            { formula: `(K${currentRow}+N${currentRow})+(K${currentRow}+N${currentRow})*(O${currentRow}/100)+M${currentRow}`},
+            objetoItem.laborRateBase ? objetoItem.laborRateBase : 0,
+            objetoItem.materialRateBase ? objetoItem.materialRateBase : 0,
+            objetoItem.equipmentRateBase ? objetoItem.equipmentRateBase : 0,
+          ]
+          if(objetoItem.categoria == "" && objetoItem.subcategoria == subcategoria) {
+            itemsArray.push(arrayFormulas)
+            currentRow++;
+          }
+        });
+      }
+    })
+    categoriasPresupuesto.forEach((categoria) => {
+      itemsArray.push(["","",categoria,"","","","","","","","","","","","","","","",""]);
+      categoriasRows.push(currentRow);
+      currentRow++;
+      items.forEach((objetoItem) => {
+        let arrayFormulas = [
+          objetoItem.L,
+          objetoItem.M,
+          objetoItem.nombre,
+          objetoItem.qty,
+          objetoItem.unidad,
+          { formula: `Q${currentRow}*A${currentRow}`, result: objetoItem.productionRate },
+          { formula: `D${currentRow}*F${currentRow}`, result: objetoItem.laborHours },
+          { formula: `IF(Y4="Government",Z4,IF(T4=0,Q${currentRow}*A${currentRow},Q${currentRow}+Q${currentRow}*T4*0.01*A${currentRow}))`},
+          { formula: `IF(U4=0,R${currentRow}*B${currentRow},R${currentRow}+R${currentRow}*U4*0.01*B${currentRow})`},
+          { formula: `IF(V4=0,S${currentRow},S${currentRow}+S${currentRow}*V4*0.01)`},
+          { formula: `IF(Y4="Private",D${currentRow}*H${currentRow},G${currentRow}*Z4)`},
+          { formula: `D${currentRow}*I${currentRow}` },
+          { formula: `D${currentRow}*J${currentRow}+(D${currentRow}*J${currentRow}*X4)`},
+          { formula: `L${currentRow}+L${currentRow}*X4`},
+          objetoItem.estSubMarkup,
+          { formula: `(K${currentRow}+N${currentRow})+(K${currentRow}+N${currentRow})*(O${currentRow}/100)+M${currentRow}`},
+          objetoItem.laborRateBase ? objetoItem.laborRateBase : 0,
+          objetoItem.materialRateBase ? objetoItem.materialRateBase : 0,
+          objetoItem.equipmentRateBase ? objetoItem.equipmentRateBase : 0,
+        ]
+        if(objetoItem.categoria == categoria && objetoItem.subcategoria == "") {
+          itemsArray.push(arrayFormulas)
+          currentRow++;
+        }
+      });
+      subcategoriasPresupuesto.forEach((subcategoria) => {
+        if(items.some(item => item.categoria == categoria && item.subcategoria == subcategoria)) {
+          itemsArray.push(["","",subcategoria,"","","","","","","","","","","","","","","",""]);
+          subcategoriasRows.push(currentRow);
+          currentRow++;
+          items.forEach((objetoItem) => {
+            let arrayFormulas = [
+              objetoItem.L,
+              objetoItem.M,
+              objetoItem.nombre,
+              objetoItem.qty,
+              objetoItem.unidad,
+              { formula: `Q${currentRow}*A${currentRow}`, result: objetoItem.productionRate },
+              { formula: `D${currentRow}*F${currentRow}`, result: objetoItem.laborHours },
+              { formula: `IF(Y4="Government",Z4,IF(T4=0,Q${currentRow}*A${currentRow},Q${currentRow}+Q${currentRow}*T4*0.01*A${currentRow}))`},
+              { formula: `IF(U4=0,R${currentRow}*B${currentRow},R${currentRow}+R${currentRow}*U4*0.01*B${currentRow})`},
+              { formula: `IF(V4=0,S${currentRow},S${currentRow}+S${currentRow}*V4*0.01)`},
+              { formula: `IF(Y4="Private",D${currentRow}*H${currentRow},G${currentRow}*Z4)`},
+              { formula: `D${currentRow}*I${currentRow}` },
+              { formula: `D${currentRow}*J${currentRow}+(D${currentRow}*J${currentRow}*X4)`},
+              { formula: `L${currentRow}+L${currentRow}*X4`},
+              objetoItem.estSubMarkup,
+              { formula: `(K${currentRow}+N${currentRow})+(K${currentRow}+N${currentRow})*(O${currentRow}/100)+M${currentRow}`},
+              objetoItem.laborRateBase ? objetoItem.laborRateBase : 0,
+              objetoItem.materialRateBase ? objetoItem.materialRateBase : 0,
+              objetoItem.equipmentRateBase ? objetoItem.equipmentRateBase : 0,
+            ]
+            if(objetoItem.categoria == categoria && objetoItem.subcategoria == subcategoria) {
+              itemsArray.push(arrayFormulas)
+              currentRow++;
+            }
+          });
+        }
+      })
+    })
 
     console.log(itemsArray)
 
@@ -56,7 +160,7 @@ export class ExcelService {
     workbook.modified = new Date();
     workbook.properties.date1904 = true;
     workbook.calcProperties.fullCalcOnLoad = true;
-    const sheet = workbook.addWorksheet('Estimate', {properties:{tabColor:{argb:'FFC0000'}}});
+    const sheet = workbook.addWorksheet('Estimate', {views: [{showGridLines: false}]});
 
     // AGREGAR TABLA PARAMETROS PRESUPUESTO
     sheet.addTable({
@@ -96,7 +200,7 @@ export class ExcelService {
       headerRow: true,
       totalsRow: false,
       style: {
-
+        theme: 'TableStyleMedium15'
       },
       columns: [
         {name: 'L'},
@@ -122,16 +226,44 @@ export class ExcelService {
       rows: itemsArray
     });
 
-    sheet.getCell('F6').alignment = { wrapText: true};
-    sheet.getCell('G6').alignment = { wrapText: true};
-    sheet.getCell('H6').alignment = { wrapText: true};
-    sheet.getCell('I6').alignment = { wrapText: true};
-    sheet.getCell('J6').alignment = { wrapText: true};
-    sheet.getCell('K6').alignment = { wrapText: true};
-    sheet.getCell('L6').alignment = { wrapText: true};
-    sheet.getCell('M6').alignment = { wrapText: true};
-    sheet.getCell('N6').alignment = { wrapText: true};
-    sheet.getCell('O6').alignment = { wrapText: true};
+    let widths = [4, 4, 50, 10, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 8, 20]
+    for (let i = 1; i < 20; i++) {
+      sheet.getColumn(i).width = widths[i-1];
+    }
+
+    let hidden = ['F','G','M','Q','R','S',];
+    hidden.forEach(element => {
+      sheet.getColumn(element).hidden = true;
+    });
+
+    let currency = ['H','I','J','K','L','M','N','P'];
+    currency.forEach(element => {
+      sheet.getColumn(element).numFmt = '"$"#,##0.00;[Red]\-"$"#,##0.00';
+    });
+
+    categoriasRows.forEach(row => {
+      let fila = sheet.getRow(row)
+      for (let i = 3; i < 17; i++) {
+        fila.getCell(i).font = {bold: true, color: {argb: 'FFFFFFFF'}, size:12};
+        fila.getCell(i).fill = {
+          type: 'pattern',
+          pattern:'solid',
+          fgColor:{argb:'00000000'},
+          bgColor:{argb:'00000000'}
+        };
+      }
+    });
+
+    subcategoriasRows.forEach(row => {
+      let fila = sheet.getRow(row)
+      for (let i = 3; i < 17; i++) {
+        fila.getCell(i).font = {italic: true, bold: true, underline: true};
+
+      }
+    });
+
+    sheet.getRow(6).alignment = { wrapText: true, horizontal: 'center'};
+    sheet.getRow(6).font = { color: {argb: 'FFFFFFFF'}};
 
     //add data and file name and download
     workbook.xlsx.writeBuffer().then((data) => {
